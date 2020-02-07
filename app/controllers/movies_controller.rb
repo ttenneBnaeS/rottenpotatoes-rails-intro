e37e48c36c08c3277c04c1f26c9860596796ce3e
ttenneBnaeS <seanerbennett@gmail.com>
2020-02-07 12:49:28 -0600

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,16 +11,23 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.order(params[:sort])
-    @sort = params[:sort]
-    # if params[:sort] == 'title'
-      # @title_header = 'hilite'
-    #   @release_date_header = ''
-    # else
-    #   @realease_date_header = 'hilite'
-    #   @title_header = ''
-    # end
-    
+
+    # Instantiate variables
+    @on = {"G" => true, "PG" => true, "PG-13" => true, "R" => true}
+    puts @on
+    @title_class = ''
+    @date = ''
+    @all_ratings = Movie.get_all_ratings
+
+    # Choose which action to perform
+    if params.has_key?(:ratings)
+      restrict
+    elsif params.has_key?(:sort)
+      sort_data
+    else
+      @movies = Movie.all
+    end
+
   end
 
   def new
@@ -50,5 +57,36 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
+
+  def sort_data
+
+    # Sort movies based on selected sort condition
+    @movies = Movie.order(params[:sort])
+    @sort = (params[:sort])
+
+    # Change color of column header that was selected
+    if @sort == 'title'
+      @title_class = 'hilite'
+      @date = ''
+    else
+      @title_class = ''
+      @date = 'hilite'
+    end 
+  end
+
+  def restrict
+    # Load requested ratings into array
+    requested_ratings = params[:ratings].keys
+
+    # Create hash of ratings which remembers which checkboxes to keep on
+    for x in @all_ratings
+      if (!requested_ratings.include? x)
+        @on[x] = false
+      end
+    end
+
+    @movies = Movie.with_ratings(requested_ratings)
+  end
+
 
 end
